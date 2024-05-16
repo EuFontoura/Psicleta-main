eaad7a86c4c2d11f7676c2028e425f5dc6ec9485
Gabriel Fontoura <gabrielFMendonca@gmail.com>
2024-05-16 19:32:51 -0300

    footer finalizado

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import ContactButton from './button/ContactButton'
 
 const Contact = () => {
   return (
-    <div className='mx-10 md:mx-20' id='Contact'>
+    <div className='mx-10 md:mx-20 my-20' id='Contact'>
       < ContactButton />
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className='bg-pink border-2 border-black/40 text-textColor'>
-      <div className='md:mx-auto md:flex justify-center text-4xl mt-10'>
+    <div className='border-2 border-black/40 text-textColor'>
+      <div className='flex justify-center mx-auto text-4xl mt-8'>
         <a href="#" className=''><FaInstagram /></a>
       </div>
       <div className='flex flex-col sm:flex-row mx-auto justify-center my-4'>
-        <a href="#Home"><p className='sm:md:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Home</p></a>
-        <a href="#Card"><p className='sm:md:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Como Podemos Ajudar</p></a>
-        <a href="#About"><p className='sm:md:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Sobre Nós</p></a>
-        <a href="#Contact"><p className='sm:md:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Contato</p></a>
+        <a href="#Home"><p className='sm:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Home</p></a>
+        <a href="#Card"><p className='sm:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Como Podemos Ajudar</p></a>
+        <a href="#About"><p className='sm:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Sobre Nós</p></a>
+        <a href="#Contact"><p className='sm:mr-20 md:text-xl hover:text-white hover:underline text-center sm:text-left'>Contato</p></a>
       </div>
-      <div className='mx-auto justify-center flex'>Copyright © 2022 - Todos os Direitos Reservados</div>
+      <div className='flex justify-center mx-auto'>Copyright © 2024 - Todos os Direitos Reservados</div>
     </div>
   );
 };
